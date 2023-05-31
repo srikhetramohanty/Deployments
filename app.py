8d995ee57052dc73c2a2ed0ef42712b935751565
srikhetramohanty <srikhetramohanty22@gmail.com>
2023-05-31 17:20:24 +0530

    Testing streamlit-flask integration

--- app.py
+++ app.py
@@ -1,41 +1,70 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
-import pandas as pd
-import pickle
-import numpy as np
-import os
-# Initalise the Flask app
-app = Flask(__name__)
-os.chdir('D:\App_')
-# Loads pre-trained model
-model = pickle.load(open('saved_model_test.pkl','rb'))
+####################################################################################
 
-@app.route('/')
+
+from flask import Flask, jsonify, request, render_template
+from torchvision import models, transforms
+from PIL import Image
+import torch
+import base64
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+# load the pre-trained model
+model = models.resnet18(pretrained=True)
+model.eval()
+
+# define the transformation for the input image
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+
+# Convert base64 to PIL image
+def base64_to_pil_image(base64_image):
+    # Decode the base64 image to bytes
+    image_bytes = base64.b64decode(base64_image)
+
+    # Create a BytesIO stream from the image bytes
+    image_stream = io.BytesIO(image_bytes)
+
+    # Open the image stream using PIL
+    pil_image = Image.open(image_stream)
+
+    return pil_image
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
-    
-    
-@app.route('/predict',methods=['POST'])
-def predict():
-    #int_features = [x for x in request.form.values()]
-    #final = np.array(int_features)
-    #prediction = np.max(final)
-    #data_unseen = pd.DataFrame([final], columns = cols)
-    #prediction = predict_model(model, data=data_unseen, round = 0)
-    #prediction = int(prediction.Label[0])
-    csv_file = request.files['data_file']
-    data = pd.read_csv(csv_file)
-    
-    #return data.shape
-    return render_template('home.html',pred='Shape of dataset {}'.format(data.shape), tables=[data.to_html(classes='data')], titles=data.columns.values)
-    
-    
-# @app.route('/predict_api',methods=['POST'])
-# def predict_api():
-#     data = request.get_json(force=True)
-#     data_unseen = pd.DataFrame([data])
-#     prediction = predict_model(model, data=data_unseen)
-#     output = prediction.Label[0]
-#     return jsonify(output)
-    
+    if request.method == 'POST':
+        # get the uploaded image from the form
+        ##img = request.files['image']
+        #print("reached here")
+        img = request.get_json()["image"]
+        #print(img)
+        # open and transform the image
+        #img = Image.open(img)
+        img = base64_to_pil_image(base64_image=img)
+        img_tensor = transform(img)
+
+        # add batch dimension to the image
+        img_tensor = img_tensor.unsqueeze(0)
+
+        # pass the image through the model
+        with torch.no_grad():
+            output = model(img_tensor)
+
+        # get the predicted class label
+        _, predicted = torch.max(output.data, 1)
+        label = predicted.item()
+        
+        return jsonify({'class_label': label})
+
+    # return a JSON response with the predicted class label
+    return render_template('home.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port="5000",debug=True)
---
+++ streamlit/app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import requests
+import json
+from PIL import Image
+from flask import Flask, jsonify, request
+
+import base64
+#from PIL import Image
+import io
+
+# Convert PIL image to base64
+def pil_image_to_base64(pil_image):
+    # Create an in-memory binary stream
+    image_stream = io.BytesIO()
+
+    # Save the PIL image to the stream in PNG format
+    pil_image.save(image_stream, format='PNG')
+
+    # Retrieve the contents of the stream
+    image_bytes = image_stream.getvalue()
+
+    # Encode the image bytes as base64
+    base64_encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+
+    return base64_encoded_image
+# from flask import Flask, jsonify, request
+# import requests
+# import json
+
+# #######################################
+# img_path = "_G6A7982.jpg"
+
+# url = "http://127.0.0.1:5000"
+
+# headers = {"Content-Type":"application/json"}
+
+# data = {"image":img_path}
+
+# print("here")
+# res = requests.post(url, headers=headers, data=json.dumps(data))
+# print(res.status_code)
+# print(res.json())
+
+def main():
+    st.title("Flask and Streamlit Integration")
+    #img_path = st.text_input("Enter some input")
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    headers = {"Content-Type":"application/json"}
+    if st.button("Submit"):
+        image = Image.open(uploaded_file)
+        img_base64 = pil_image_to_base64(pil_image=image)
+        data = {"image":img_base64}
+        print("here")
+        res = requests.post("http://127.0.0.1:5000", headers=headers, data=json.dumps(data))
+        print(res.status_code)
+        print(res.json())
+        st.write("Flask app response:", res.json())
+
+if __name__ == "__main__":
+    main()
