c8a480d2e1011a303e366d8450f0035ff30a4d2d
srikhetramohanty <srikhetramohanty22@gmail.com>
2023-05-31 17:51:15 +0530

    run without main function clause

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def home():
     # return a JSON response with the predicted class label
     return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port="5000",debug=True)
+#if __name__ == '__main__':
+app.run(host="0.0.0.0",port="5000",debug=True)
