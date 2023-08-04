de37ce6b3e439fb9e05684d2e5405eefceaa2ad9
srikhetramohanty <srikhetramohanty22@gmail.com>
2023-08-04 18:50:02 +0530

    yolov8 static model v1

--- app.py
+++ app.py
@@ -4,20 +4,18 @@
 from flask import Flask, jsonify, request, render_template
 from torchvision import models, transforms
 from PIL import Image
-import torch
+from ultralytics import YOLO
 
 app = Flask(__name__)
 
-# load the pre-trained model
-model = models.resnet18(pretrained=True)
-model.eval()
+# Load a pretrained YOLOv8n model
+model = YOLO('yolov8n.pt')
+print('Model loaded....')
 
 # define the transformation for the input image
 transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Resize((640,640)),
+    transforms.ToTensor()
 ])
 
 @app.route('/', methods=['GET', 'POST'])
@@ -28,22 +26,35 @@ def home():
         #print("reached here")
         #img = request.get_json()["image"]
         #print(img)
-        # open and transform the image
+        #open and transform the image
         img = Image.open(img)
         img_tensor = transform(img)
 
-        # add batch dimension to the image
+        #add batch dimension to the image
         img_tensor = img_tensor.unsqueeze(0)
 
-        # pass the image through the model
-        with torch.no_grad():
-            output = model(img_tensor)
+        ##########################################
 
-        # get the predicted class label
-        _, predicted = torch.max(output.data, 1)
-        label = predicted.item()
+
+        # Define path to the image file
+        source = img_tensor
+        #print(img)
+        #print(source)
+
+        # Run inference on the source
+        results = model(source)  # list of Results objects
+        ##########################################
+
+        # # pass the image through the model
+        # with torch.no_grad():
+        #     output = model(img_tensor)
+
+        # # get the predicted class label
+        # _, predicted = torch.max(output.data, 1)
+        # label = predicted.item()
         
-        return jsonify({'class_label': label})
+        #return jsonify({'class_label': label})
+        return results[0].tojson()
 
     # return a JSON response with the predicted class label
     return render_template('home.html')
